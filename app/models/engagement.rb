d5d6a6320165bd1c849227fb62af961d1f7aa7ff
Phil Vargas <philipvargas@thisisartium.com>
2022-10-07 14:01:46 -0700

    feat(Engagement) create initial model

---
+++ app/models/engagement.rb
@@ -0,0 +1,2 @@
+class Engagement < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_205305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "engagement_status", ["UNSTARTED", "ACTIVE", "COMPLETE"]
+
+  create_table "engagements", force: :cascade do |t|
+    t.string "name", null: false
+    t.enum "status", default: "UNSTARTED", null: false, enum_type: "engagement_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
